8321caedfeee8261a43ed03a8e1ea79fb2a74cd4
Jubaer007 <skjubaer786@gmail.com>
2022-04-11 14:17:28 +0600

    minor changes

--- news.js
+++ news.js
@@ -2,18 +2,25 @@ var dropdown = document.querySelectorAll(".dropdown");
 var dropItem = dropdown[0].querySelectorAll(".dropdown-menu");
 dropdown[0].addEventListener('mouseover', function(){
     dropItem[0].style.display="block";
-})
+});
 
 dropdown[0].addEventListener('mouseout', function(){
-    dropItem[0].style.display="none";
-})
+    setTimeout(function(){
+        dropItem[0].style.display="none";
+    },500);
+});
 
 
 var dropItem2 = dropdown[1].querySelectorAll(".dropdown-menu");
 dropdown[1].addEventListener('mouseover', function(){
     dropItem2[0].style.display="block";
-})
+    dropItem[0].style.transitionDuration = '3s';
+});
 
 dropdown[1].addEventListener('mouseout', function(){
-    dropItem2[0].style.display="none";
-})
+    setTimeout(function(){
+        dropItem2[0].style.display="none";
+        dropItem[0].style.transitionDuration = '3s';
+    },500);
+    
+});
